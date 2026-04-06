47050f76344cb1ee2d2da507ee60c9a987769af9
John Louis Policarpio <106532350+nloudev@users.noreply.github.com>
2026-04-07 01:07:01 +1000

    Feat: Order Button Functionality

--- src/server.js
+++ src/server.js
@@ -1,20 +1,27 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import orderRoutes from './routes/orders.js';
 import productRoutes from './routes/products.js';
 import healthRoutes from './routes/health.js';
 import docsRoutes from './routes/docs.js';
 import buyerRoutes from './routes/buyers.js'
 import sellerRoutes from './routes/sellers.js'
+import emailRoutes from './routes/email.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/orders', orderRoutes);
+app.use('/orders', emailRoutes);
 app.use('/products', productRoutes);
 app.use('/health', healthRoutes);
 app.use('/api-docs', docsRoutes);
